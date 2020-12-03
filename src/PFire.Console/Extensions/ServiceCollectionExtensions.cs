253ebaaa40aa9a990c7f1b2ea0c209c21fe7c042
Joe Cellucci <celluj34@gmail.com>
2020-12-03 15:02:51 -0600

    add logging extensions to Console project

--- src/PFire.Console/Extensions/ServiceCollectionExtensions.cs
+++ src/PFire.Console/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PFire.Common.Extensions;
 using PFire.Console.Services;
 using PFire.Core.Extensions;
 using PFire.Infrastructure.Extensions;
@@ -11,10 +12,11 @@ namespace PFire.Console.Extensions
     {
         public static IServiceCollection RegisterAll(this IServiceCollection serviceCollection, IHostEnvironment hostEnvironment, IConfiguration configuration)
         {
-            return serviceCollection
-                   .AddHostedService<PFireServerService>()
-                   .RegisterCore()
-                   .RegisterInfrastructure(hostEnvironment, configuration);
+            return serviceCollection.AddHostedService<PFireServerService>()
+                                    .RegisterCore()
+                                    .RegisterInfrastructure()
+                                    .RegisterCommon(configuration)
+                                    .AddLogging(builder => builder.AddLogging(hostEnvironment));
         }
     }
 }
