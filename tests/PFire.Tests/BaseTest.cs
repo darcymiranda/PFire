281e1d682b8fd4f380df1bf96df9d2b82afec2e5
Joe Cellucci <celluj34@gmail.com>
2020-12-03 12:44:06 -0600

    add PFireServerServiceTests

---
+++ tests/PFire.Tests/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Moq.AutoMock;
+
+namespace PFire.Tests
+{
+    public abstract class BaseTest
+    {
+        // ReSharper disable once InconsistentNaming
+        protected readonly AutoMocker _autoMoqer;
+
+        // ReSharper disable once InconsistentNaming
+        protected readonly Random _random;
+
+        protected BaseTest()
+        {
+            _autoMoqer = new AutoMocker();
+            _random = new Random();
+        }
+    }
+}
