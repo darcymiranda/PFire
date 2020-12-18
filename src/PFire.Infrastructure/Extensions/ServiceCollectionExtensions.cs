477c490cddf08b821fe7fac41f881fdc034832d5
Oscar Veldman <oveldman@gmail.com>
2020-12-18 20:17:49 +0100

    Fix memory SQLLite

--- src/PFire.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/PFire.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -2,18 +2,28 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PFire.Infrastructure.Services;
+using Microsoft.Data.Sqlite; 
 
 namespace PFire.Infrastructure.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        public const bool openConnectionNeeded = false;
+
         public static IServiceCollection RegisterInfrastructure(this IServiceCollection serviceCollection, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("PFire");
+            SqliteConnection connection = new(connectionString);
+
+            if (openConnectionNeeded) {
+                #pragma warning disable CS0162
+                connection.Open();  
+                #pragma warning restore CS0162
+            }
 
             return serviceCollection.AddScoped<IDatabaseContext, DatabaseContext>()
                                     .AddScoped<IDatabaseMigrator, DatabaseContext>()
-                                    .AddDbContext<DatabaseContext>(options => options.UseSqlite(connectionString));
+                                    .AddDbContext<DatabaseContext>(options => options.UseSqlite(connection));
         }
     }
 }
