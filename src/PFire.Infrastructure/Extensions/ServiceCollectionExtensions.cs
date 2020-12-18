0b9b3c541c290bbfa93978e990e1fde55b6305a2
Oscar Veldman <oveldman@gmail.com>
2020-12-18 21:01:25 +0100

    Add settings part 2

--- src/PFire.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/PFire.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,25 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PFire.Infrastructure.Services;
 using Microsoft.Data.Sqlite; 
+using PFire.Infrastructure.Models;
+using PFire.Infrastructure.Services;
 
 namespace PFire.Infrastructure.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public const bool openConnectionNeeded = false;
+        private const string ServerSettings = "ServerSettings";
 
         public static IServiceCollection RegisterInfrastructure(this IServiceCollection serviceCollection, IConfiguration configuration)
         {
+            ServerSettings serverSettings = new(); 
+            configuration.GetSection(ServerSettings).Bind(serverSettings);
+
             var connectionString = configuration.GetConnectionString("PFire");
             SqliteConnection connection = new(connectionString);
 
-            if (openConnectionNeeded) {
-                #pragma warning disable CS0162
+            System.Console.WriteLine(serverSettings.OpenSqlConnection);
+
+            if (serverSettings.OpenSqlConnection) {
                 connection.Open();  
-                #pragma warning restore CS0162
             }
+
+            serviceCollection.Configure<ServerSettings>(configuration.GetSection(ServerSettings));
 
             return serviceCollection.AddScoped<IDatabaseContext, DatabaseContext>()
                                     .AddScoped<IDatabaseMigrator, DatabaseContext>()
---
+++ src/PFire.Infrastructure/Models/ServerSettings.cs
@@ -0,0 +1,7 @@
+namespace PFire.Infrastructure.Models
+{
+    public class ServerSettings
+    {
+        public bool OpenSqlConnection { get; set;}
+    }
+}
