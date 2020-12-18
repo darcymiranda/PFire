da83f066ad8980edbcf2fa816894b1c7f5ec5449
Oscar Veldman <oveldman@gmail.com>
2020-12-18 06:37:17 +0100

    Security Update login. Password hashing. (#24)
    * Remove some unnecessary static methodes and add one type safety at User.Equals()
    
    * Fix password validation failed.
    
    * Revert "Fix password validation failed."
    
    This reverts commit b456a361e1b47890b921f5f4e3525ad7c516aa92.
    
    * Add extra security layer serverside password hash.
    
    * Merge set good

--- src/PFire.Core/Protocol/Messages/Inbound/LoginRequest.cs
+++ src/PFire.Core/Protocol/Messages/Inbound/LoginRequest.cs
@@ -22,7 +22,7 @@ namespace PFire.Core.Protocol.Messages.Inbound
             var user = await context.Server.Database.QueryUser(Username);
             if (user != null)
             {
-                if (user.Password != Password)
+                if (!BCrypt.Net.BCrypt.Verify(Password, user.Password))
                 {
                     await context.SendAndProcessMessage(new LoginFailure());
 
@@ -31,7 +31,8 @@ namespace PFire.Core.Protocol.Messages.Inbound
             }
             else
             {
-                user = await context.Server.Database.InsertUser(Username, Password, context.Salt);
+                var hashPassword = BCrypt.Net.BCrypt.HashPassword(Password);
+                user = await context.Server.Database.InsertUser(Username, hashPassword, context.Salt);
             }
 
             // Remove any older sessions from this user (duplicate logins)
