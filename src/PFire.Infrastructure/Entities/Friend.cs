f5f5071a819761476417cba0bcc246fdd14983b5
Joe Cellucci <celluj34@gmail.com>
2020-12-19 22:14:19 -0600

    Fix Entity Framework (#34)
    * fix connection string
    
    * move configuration to own files
    
    * add migration for fixing table names
    
    * fix null-check

--- src/PFire.Infrastructure/Entities/Friend.cs
+++ src/PFire.Infrastructure/Entities/Friend.cs
@@ -1,4 +1,7 @@
-﻿namespace PFire.Infrastructure.Entities
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PFire.Infrastructure.Entities
 {
     public class Friend : Entity
     {
@@ -11,5 +14,38 @@
         public string Message { get; set; }
 
         public bool Pending { get; set; }
+    }
+
+    internal class FriendConfiguration : EntityConfiguration<Friend>
+    {
+        protected override void ConfigureEntity(EntityTypeBuilder<Friend> builder)
+        {
+            builder.ToTable(nameof(Friend));
+
+            builder.HasKey(x => new
+            {
+                RequesterId = x.MeId,
+                RecipientId = x.ThemId
+            });
+
+            builder.HasIndex(x => new
+                   {
+                       x.MeId,
+                       x.ThemId
+                   })
+                   .IsUnique();
+
+            builder.HasOne(x => x.Me)
+                   .WithMany(x => x.MyFriends)
+                   .HasForeignKey(x => x.MeId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Them)
+                   .WithMany(x => x.FriendsOf)
+                   .HasForeignKey(x => x.ThemId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Property(x => x.Message).HasMaxLength(1000);
+        }
     }
 }
--- src/PFire.Infrastructure/Migrations/20201219103931_FixTableNames.Designer.cs
+++ src/PFire.Infrastructure/Migrations/20201219103931_FixTableNames.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PFire.Infrastructure.Services;
 
 namespace PFire.Infrastructure.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20201219103931_FixTableNames")]
+    partial class FixTableNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
