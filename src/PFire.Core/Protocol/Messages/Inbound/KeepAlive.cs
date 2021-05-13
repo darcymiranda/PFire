885d163857c09f428113b690e6360c64fd103f71
Darcy Miranda <d.miranda@hippocmms.com>
2021-05-13 17:59:19 -0500

    Hide keep alive warnings

--- src/PFire.Core/Protocol/Messages/Inbound/KeepAlive.cs
+++ src/PFire.Core/Protocol/Messages/Inbound/KeepAlive.cs
@@ -1,7 +1,15 @@
-﻿namespace PFire.Core.Protocol.Messages.Inbound
+﻿using System.Threading.Tasks;
+using PFire.Core.Session;
+
+namespace PFire.Core.Protocol.Messages.Inbound
 {
     internal sealed class KeepAlive : XFireMessage
     {
         public KeepAlive() : base(XFireMessageType.KeepAlive) {}
+
+        public override Task Process(IXFireClient client)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
