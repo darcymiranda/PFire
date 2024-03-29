017656ec649739f6ce807284b683265119084a5f
desukuran <dethklan@gmail.com>
2024-03-29 10:43:52 -0400

    Packets 21 and 149 - Fetch Friend Favorite Game Server List (#46)
    * Preliminary adding, it reads and sends as it should but no real data is sent, just enough to satisfy xfire awaiting something.
    * This packet deals with fetching all game server information based on Game ID and Friend Favorites.
    
    Added Inbound Packet GameServerFetchFriendsFavorites (21)
     * It only expects a gameid. From there the server sends back GameServerSendFriendsFavorites packet. (Weirdly enough, this one uses the string "gameid" unlike the All packet.)
    
    Added Outbound Packet GameServerSendFriendsFavorites (149)
     * Four attributes are sent: gameid, a list of gameips and a list of gameports (both iterates in sync), list of a list of ints for FriendIds.
     * I have added as such for future addition: It makes the lists but doesn't populate them and sends back the GameId sent by the client.
     * The FriendIds nested list is a weird one, only ever used again in FriendofFriends. I had to make a list of userids and then put it inside of FriendIds. I don't know why they designed it as such, as vanilla xfire doesn't use this information at all it seems...
    
    XFireMessageType and XFireMessageTypeFactory adds the packets to the lists as needed.

---
+++ src/PFire.Core/Protocol/Messages/Inbound/GameServerFetchFriendsFavorites.cs
@@ -0,0 +1,19 @@
+﻿using PFire.Core.Protocol.Messages.Outbound;
+using PFire.Core.Session;
+using System.Threading.Tasks;
+
+namespace PFire.Core.Protocol.Messages.Inbound
+{
+    internal sealed class GameServerFetchFriendsFavorites : XFireMessage
+    {
+        public GameServerFetchFriendsFavorites() : base(XFireMessageType.GameServerFetchFriendsFavorites) { }
+
+        [XMessageField("gameid")]
+        public int GameId { get; set; }
+
+        public override async Task Process(IXFireClient context)
+        {
+            await context.SendAndProcessMessage(new GameServerSendFriendsFavorites(GameId));
+        }
+    }
+}
