e11ee0e5f410742bc7d5aff86fd66aec798467a7
desukuran <dethklan@gmail.com>
2024-03-29 10:44:35 -0400

    Packet 169 - System Broadcast (#47)
    This packet is sent from the server to send a broadcast to all logged in clients.
    
    It features two attributes: An unknown int and a message.
    
    As noted in the comments, I am not sure what the int sent does. I looked at decompiled code and it seems to make it skip over reading a string if set to zero or something. However even when set to zero it still displays.

---
+++ src/PFire.Core/Protocol/Messages/Outbound/SystemBroadcastMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PFire.Core.Protocol.Messages.Outbound
+{
+    internal sealed class SystemBroadcastMessage : XFireMessage
+    {
+        public SystemBroadcastMessage(string message) : base(XFireMessageType.SystemBroadcast)
+        {
+            Message = message;
+        }
+
+        // Not entirely sure what this does. I looked at decompiled code and it seems to make it
+        // skip over reading a string if set to zero or something. However even when set to zero
+        // it still displays.
+        [XMessageField(0x34)]
+        public int Unk { get; set; }
+
+        [XMessageField(0x2E)]
+        public string Message { get; set; }
+    }
+}
