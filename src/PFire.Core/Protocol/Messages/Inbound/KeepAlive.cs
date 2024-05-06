e9c9cfa113e00dc5e8a73d11e015d999e64061cc
Scott Riordan <scott.riordan@norimaconsulting.com>
2024-05-05 20:14:10 -0500

    Add missing KeepAlive props + unit test

--- src/PFire.Core/Protocol/Messages/Inbound/KeepAlive.cs
+++ src/PFire.Core/Protocol/Messages/Inbound/KeepAlive.cs
@@ -1,13 +1,19 @@
-﻿using System.Threading.Tasks;
-using PFire.Core.Session;
-using PFire.Core.Protocol.Messages;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using PFire.Core.Protocol.Messages.Outbound;
+using PFire.Core.Session;
 
 namespace PFire.Core.Protocol.Messages.Inbound
 {
     internal sealed class KeepAlive : XFireMessage
     {
-        public KeepAlive() : base(XFireMessageType.KeepAlive) {}
+        public KeepAlive() : base(XFireMessageType.KeepAlive) { }
+
+        [XMessageField("value")]
+        public int? Value { get; set; }
+
+        [XMessageField("stats")]
+        public List<object> Stats { get; set; }
 
         public async override Task Process(IXFireClient client)
         {
